fb5f83cacb556e150f76bd62e399d4cedc1ef1ec
Seth Lawler <slawler@gmu.edu>
2016-11-30 08:27:32 -0500

    Update Grid2MOD_file.py

--- hms/Grid2MOD_file.py
+++ hms/Grid2MOD_file.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Nov 24 12:00:26 2015
-Create a grid file for HEC-HMS, requires some manual editing of .mod file
+Create a grid file for HEC-HMS
 @author: slawler
 
 Example File:
@@ -20,18 +20,31 @@ Subbasin: 2C2
      GridCell: 599 257 0.15554718877992446 0.000088
 End:
 
-
 """
 
+
+#---Required python modules
 import pandas as pd
+import os
 
-infile = 'table.txt'
-outfile= 'HecModel.mod'
-df = pd.read_csv(infile, sep = '\t')
 
-sub_list = df.Name.sort_values()
-subs = sub_list.unique()
+#---Assign Directory & Path Names
 
+wdir = r"C:\Users\slawler\Documents\LAMP\TEMP" # File Directory
+hrap_to_hms_table = 'HRAP_for_HMS.txt'         # Table from ArcGIS
+hms_model = 'HecModel'                         # Name of HMS model
+Subbasin = 'Name'                              # Column name of the Subbasins 
+hrap_x   = 'X_int'                             # Column name of the HRAP X GridCell 
+hrap_y   = 'Y_int'                             # Column name of the HRAP Y GridCell
+area     = 'Area_sqkm'                         # Column name of the Area in Square Kilometers
+
+
+# Run Script
+f = os.path.join(wdir,hrap_to_hms_table ) # Create a full path to the file
+df = pd.read_csv(f, sep = ',')            # Read in data         
+subs = df[Subbasin].unique() # Unique Subbasins dissolved during intersect w/HRAP grid
+
+#---HMS Formatted data 
 header = 'Parameter Order: Xcoord Ycoord TravelLength Area'
 trvlng = '1.00000000000000000'
 space = ' '
@@ -39,21 +52,22 @@ init = '     GridCell: '
 end = 'End: \n'
 subbasin = 'Subbasin: '
 
-with open(outfile,'w') as f:
+#  Write .mod file
+modfile = os.path.join(wdir, hms_model + '.mod')
+
+with open(modfile,'w') as f:
     for i, s in enumerate(subs):
         if i == 0:
             f.write(header + '\n')
             f.write(end)
-        idx = df.ix[df['Name'] == s].index.tolist()
+        idx = df.ix[df[Subbasin] == s].index.tolist()
         f.write('\n'+subbasin+ s + '\n')
         for j in idx:
-            basin = df['Name'].iloc[j]
-            x = df['X'].iloc[j]
-            y = df['Y'].iloc[j]
-            a = df['AreaSqKm'].iloc[j]
+            basin = df[Subbasin].iloc[j]
+            x = df[hrap_x].iloc[j]
+            y = df[hrap_y ].iloc[j]
+            a = df[area].iloc[j]
             line = init + str(x) +space+ str(y) +space+ trvlng +space+ str(a)
             f.write(line + '\n')
         f.write(end)
-
-
 
